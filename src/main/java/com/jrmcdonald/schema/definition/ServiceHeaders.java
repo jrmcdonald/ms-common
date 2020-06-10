611989f4f9062d00477ac0db1c67ded4b5e080da
Jamie McDonald <jamie.r.mcdonald@waitrose.co.uk>
2020-06-10 09:40:20 +0100

    refactor: add private constructor

--- src/main/java/com/jrmcdonald/schema/definition/ServiceHeaders.java
+++ src/main/java/com/jrmcdonald/schema/definition/ServiceHeaders.java
@@ -1,5 +1,10 @@
 package com.jrmcdonald.schema.definition;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ServiceHeaders {
+
     public static final String CUSTOMER_ID = "customer-id";
 }
